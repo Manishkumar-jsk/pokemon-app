341d8944231fda681bc9fa0ded03c423162a0d1d
Manishkumar-jsk <manishsharma65263@gmail.com>
2025-06-12 19:09:44 +0530

    fixed build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 //components
+import { Suspense } from "react";
 import PokemonForm from "./components/PokemonForm";
 import PokemonList from "./components/PokemonList";
+import Loader from "./components/Loader";
 
 export default function Home() {
   return (
     <div>
-      <PokemonForm />
-      <PokemonList />
+       <Suspense fallback={<Loader />}>
+        <PokemonForm />
+      </Suspense>
+      <Suspense fallback={<Loader />}>
+        <PokemonList />
+      </Suspense>
     </div>
   );
 }
